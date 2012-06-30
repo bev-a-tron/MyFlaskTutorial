e66033d2785329028215b3f5fbb12e648c9938d2
bev-a-tron <beverly.a.lau@gmail.com>
2012-06-30 14:38:18 -0400

    updated application_lulu.py to be complete

--- application_lulu.py
+++ application_lulu.py
@@ -1,12 +1,22 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 app_lulu = Flask(__name__)
 
 app_lulu.vars={}
 
+app_lulu.questions={}
+app_lulu.questions['How many eyes do you have?']=('1','2','3')
+app_lulu.questions['Which fruit do you like best?']=('banana','mango','pineapple')
+app_lulu.questions['Do you like cupcakes?']=('yes','no','maybe')
+
+app_lulu.nquestions=len(app_lulu.questions)
+#should be 3
+
 @app_lulu.route('/index_lulu',methods=['GET','POST'])
 def index_lulu():
-    nquestions=5
+    nquestions=app_lulu.nquestions
     if request.method == 'GET':
+        #initialize question number
+        app_lulu.num=0
         return render_template('userinfo_lulu.html',num=nquestions)
     else:
         #request was a POST
@@ -18,7 +28,46 @@ def index_lulu():
         f.write('Age: %s\n\n'%(app_lulu.vars['age']))
         f.close()
 
-        return render_template('layout_lulu.html',num=1,question='How many eyes do you have?',ans1='1',ans2='2',ans3='3')
+        return redirect('/main_lulu')
+        #return render_template('layout_lulu.html',num=1,question='How many eyes do you have?',ans1='1',ans2='2',ans3='3')
+
+@app_lulu.route('/main_lulu')
+def main_lulu2():
+    if len(app_lulu.questions)==0 : return render_template('end_lulu.html')
+    app_lulu.num+=1 # app_lulu.num = app_lulu.num + 1
+    return redirect(url_for('next_lulu'))
+
+#####################################
+## IMPORTANT: I have separated /next_lulu INTO GET AND POST
+## You can also do this in one function, with If and Else.
+
+@app_lulu.route('/next_lulu',methods=['GET'])
+def next_lulu():  #remember the function name does not need to match the URL
+    # for clarity (temp variables):
+    n=app_lulu.num
+    q=app_lulu.questions.keys()[0] #python indexes at 0
+    a1=app_lulu.questions[q][0]
+    a2=app_lulu.questions[q][1]
+    a3=app_lulu.questions[q][2]
+
+    # save current question
+    app_lulu.currentq=q
+
+    return render_template('layout_lulu.html',num=n,question=q,ans1=a1,ans2=a2,ans3=a3)
+
+@app_lulu.route('/next_lulu',methods=['POST'])
+def next_lulu2():  #can't have two functions with the same name
+    # Here, we will collect data from the user.
+    # Then, we return to the main function, so it can tell us whether to 
+    # display another question page, or to show the end page.
+
+    f=open('%s_%s.txt'%(app_lulu.vars['name'],app_lulu.vars['age']),'a') #a is for append
+    f.write('%s\n'%(app_lulu.currentq))
+    f.write('%s\n\n'%(request.form['answer_lulu'])) #do you know where answer_lulu comes from?
+    f.close()
+    app_lulu.questions.pop(app_lulu.currentq)
+
+    return redirect('main_lulu')
 
 if __name__ == "__main__":
     app_lulu.run()
